4504e1df6156433c215eeb0e28896f90b091494a
Nikola Arlotti <aksamitna.egzystencja@gmail.com>
2025-03-05 21:42:00 +0100

    removed trailing whitespace

--- mini-grep.py
+++ mini-grep.py
@@ -4,11 +4,11 @@ import argparse
 import re as regex
 import fileinput
 import sys
-  
+
 
 def search(file_paths, pattern, line_nums):
     result = []
-    try: 
+    try:
         with fileinput.input(files=file_paths) as text:
             for line in text:
                 if regex.search(pattern, line):
@@ -16,8 +16,8 @@ def search(file_paths, pattern, line_nums):
                         result.append(line.strip())
                     else:
                         result.append(''.join([
-                                            str(text.filelineno()), 
-                                            " ", 
+                                            str(text.filelineno()),
+                                            " ",
                                             line.strip()]))
             if not result:
                 result.append('No occurencce of PATTERN found in file.')
@@ -25,7 +25,7 @@ def search(file_paths, pattern, line_nums):
     except FileNotFoundError as error:
         result.append('Error: File not found')
         return result
-    
+
 
 def compile_regex(pattern):
     try:
@@ -46,27 +46,27 @@ if __name__ == '__main__':
     )
 
     argparser.add_argument(
-        '-q', 
-        action='store_false', 
+        '-q',
+        action='store_false',
         help='If this option is given, mini-grep will only output lines, \
             omitting the matching line numbers.')
     argparser.add_argument(
-        '-e', 
-        metavar='PATTERN', 
-        required=True, 
+        '-e',
+        metavar='PATTERN',
+        required=True,
         help='Pattern to scan files for. Has to be a valid \
             regular expression. Required argument.')
     argparser.add_argument(
-        'FILE', 
-        nargs='*', 
-        default=[], 
+        'FILE',
+        nargs='*',
+        default=[],
         help='Zero or more files to scan for the pattern given with -e. \
             If number of files is zero, standard input will be scanned.')
 
     args = argparser.parse_args()
-    
+
     pattern = compile_regex(args.e)
-    
+
     if not pattern:
         print('Regular expression error')
         sys.exit(1)
--- test.py
+++ test.py
@@ -12,17 +12,17 @@ class TestMiniGrep(unittest.TestCase):
         self.assertEqual(
             search('./test_texts/bible', '\\svile\\s', True),
             [
-            '17965 should seem vile unto thee.', 
-            '23098 good unto you: but unto this man do not so vile a thing.', 
-            '27241 6:22 And I will yet be more vile than thus, and will be base in mine', 
-            '44399 18:3 Wherefore are we counted as beasts, and reputed vile in your', 
-            '46458 15:4 In whose eyes a vile person is contemned; but he honoureth them', 
-            '58899 32:5 The vile person shall be no more called liberal, nor the churl', 
-            '58902 32:6 For the vile person will speak villany, and his heart will work', 
-            '63807 them like vile figs, that cannot be eaten, they are so evil.',  
-            '72543 11:21 And in his estate shall stand up a vile person, to whom they', 
-            '90483 1:26 For this cause God gave them up unto vile affections: for even', 
-            '94578 Christ: 3:21 Who shall change our vile body, that it may be fashioned', 
+            '17965 should seem vile unto thee.',
+            '23098 good unto you: but unto this man do not so vile a thing.',
+            '27241 6:22 And I will yet be more vile than thus, and will be base in mine',
+            '44399 18:3 Wherefore are we counted as beasts, and reputed vile in your',
+            '46458 15:4 In whose eyes a vile person is contemned; but he honoureth them',
+            '58899 32:5 The vile person shall be no more called liberal, nor the churl',
+            '58902 32:6 For the vile person will speak villany, and his heart will work',
+            '63807 them like vile figs, that cannot be eaten, they are so evil.',
+            '72543 11:21 And in his estate shall stand up a vile person, to whom they',
+            '90483 1:26 For this cause God gave them up unto vile affections: for even',
+            '94578 Christ: 3:21 Who shall change our vile body, that it may be fashioned',
             '96960 goodly apparel, and there come in also a poor man in vile raiment; 2:3'
             ]
         )
@@ -42,19 +42,19 @@ class TestMiniGrep(unittest.TestCase):
     def test_file_not_found(self):
         self.assertEqual(search('./test_texts/boble', '\\sGod\\s', False),
                          ['Error: File not found'])
-        
+
     def test_multiple_files(self):
         self.assertEqual(
             search(['test_texts/tadeusz', 'test_texts/bible'], 'GUTENBERG', True),
             [
-            '25 *** START OF THE PROJECT GUTENBERG EBOOK PAN TADEUSZ ***', 
-            '5419 *** END OF THE PROJECT GUTENBERG EBOOK PAN TADEUSZ ***', 
-            '5433 Gutenberg™ electronic works to protect the PROJECT GUTENBERG™', 
-            '5449 THE FULL PROJECT GUTENBERG LICENSE', 
+            '25 *** START OF THE PROJECT GUTENBERG EBOOK PAN TADEUSZ ***',
+            '5419 *** END OF THE PROJECT GUTENBERG EBOOK PAN TADEUSZ ***',
+            '5433 Gutenberg™ electronic works to protect the PROJECT GUTENBERG™',
+            '5449 THE FULL PROJECT GUTENBERG LICENSE',
             '20 *** START OF THE PROJECT GUTENBERG EBOOK THE KING JAMES VERSION OF THE BIBLE ***',
             '99618 *** END OF THE PROJECT GUTENBERG EBOOK THE KING JAMES VERSION OF THE BIBLE ***',
-            '99632 Gutenberg™ electronic works to protect the PROJECT GUTENBERG™', 
-            '99648 THE FULL PROJECT GUTENBERG LICENSE', 
+            '99632 Gutenberg™ electronic works to protect the PROJECT GUTENBERG™',
+            '99648 THE FULL PROJECT GUTENBERG LICENSE',
             ]
         )
 
@@ -77,7 +77,7 @@ class TestMiniGrep(unittest.TestCase):
             output,
             'usage: mini-grep [-h] [-q] -e PATTERN [FILE ...]\nmini-grep: error: unrecognized arguments: -r'
             )
-        
+
         output = subprocess.getoutput(
                 ['./mini-grep.py ./test_texts/tadeusz'],
                 )
